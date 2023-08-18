53d8590b35e5492cf34ccf92ca26ea79a3546e6f
Daniel Araya <darayaroma@gmail.com>
2023-08-18 00:49:29 -0600

    [feat] client consuming soap

--- evacomuna/src/main/java/cr/ac/una/util/RequestExample.java
+++ evacomuna/src/main/java/cr/ac/una/util/RequestExample.java
@@ -1,27 +1,15 @@
 package cr.ac.una.util;
 
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-
-import cr.ac.una.model.User;
-import cr.ac.una.services.UserService;
-import static cr.ac.una.config.Server.REMOTE_SERVER_URL;
+import cr.ac.una.services.*;
 
 public class RequestExample {
     public static void request() {
-        String serviceUrl = REMOTE_SERVER_URL;
+        User_Service userService = new User_Service();
+        UserServiceImpl port = userService.getUserServiceImplPort();
+        GetUser request = new GetUser();
+        request.setArg0(1);
 
-        // Create a JaxWsProxyFactoryBean
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setServiceClass(UserService.class); // Define your service interface
-        factory.setAddress(serviceUrl + "/user");
-
-        // Create the client proxy
-        UserService client = (UserService) factory.create();
-
-        // Make the SOAP request using the client proxy
-        User user = client.getUser(1);
-
-        // Process the response
-        System.out.println("Response: " + user.toString());
+        User user = port.getUser(1);
+        System.out.println("UserID: " + user.getId() + "Name: " + user.getName());
     }
 }
