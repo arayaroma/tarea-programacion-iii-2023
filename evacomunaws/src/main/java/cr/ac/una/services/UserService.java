1d91a333a46b2264ff81ac0cf2c10df1c2f79c32
Daniel Araya <darayaroma@gmail.com>
2023-08-19 18:49:25 -0600

    [refactor] Project structure

--- evacomunaws/src/main/java/cr/ac/una/services/UserService.java
+++ evacomunaws/src/main/java/cr/ac/una/services/UserService.java
@@ -1,11 +1,19 @@
 package cr.ac.una.services;
 
-import cr.ac.una.model.User;
+import cr.ac.una.entities.User;
 import jakarta.jws.WebMethod;
-import jakarta.jws.WebService;
-
 
 public interface UserService {
+    @WebMethod(operationName = "createUser")
+    public User createUser(User user);
+
     @WebMethod(operationName = "getUser")
-    public User getUser(int id);
+    public User getUserById(Long id);
+
+    @WebMethod(operationName = "updateUser")
+    void updateUser(User user);
+
+    @WebMethod(operationName = "deleteUser")
+    void deleteUser(Long id);
+
 }
