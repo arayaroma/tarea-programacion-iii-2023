f64958af1c60f5bd22826dc8ef5a451b24f818cb
enajera123 <estebannajera42@gmail.com>
2023-08-21 11:22:24 -0600

    add/wsdl
    agregue mis propios wsdl

--- evacomuna/src/main/java/cr/ac/una/controller/LoginController.java
+++ evacomuna/src/main/java/cr/ac/una/controller/LoginController.java
@@ -22,8 +22,6 @@ import javafx.scene.layout.HBox;
 public class LoginController implements Initializable {
 
     @FXML
-    private TextField txfUser;
-    @FXML
     private PasswordField txfPassword;
     @FXML
     private TextField txfUserRegister;
@@ -34,19 +32,26 @@ public class LoginController implements Initializable {
     @FXML
     private TextField txfLastNameRegister;
     @FXML
-    private TextField txfPhoneRegister;
-    @FXML
     private TextField txfEmailRegister;
     @FXML
-    private ComboBox<?> cbRoleRegister;
+    private TextField txfCedRegister;
+    @FXML
+    private TextField txfSecondLastNameRegister;
+    @FXML
+    private TextField txfPhoneNumberRegister;
+    @FXML
+    private TextField txfUser;
+    @FXML
+    private ComboBox<String> cbRoleRegister;
     @FXML
     private HBox loginView;
     @FXML
     private HBox registerView;
     @FXML
     private ImageView imgPhoto;
-    
+
     private User userService = new User();
+
     /**
      * Initializes the controller class.
      */
@@ -74,24 +79,26 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    private void choosePhotoProfile(ActionEvent event) {
-    }
-
-    @FXML
     private void registerUser(ActionEvent event) {
-        String userName = txfUserRegister.getText(), password = txfPasswordRegister.getText(), name = txfNameRegister.getText(), lastName = txfLastNameRegister.getText(), phoneNumber = txfPhoneRegister.getText(), email = txfEmailRegister.getText();
-        if (userName.isBlank() || password.isBlank() || name.isBlank() || lastName.isBlank() || phoneNumber.isBlank() || email.isBlank()) {
+        String userName = txfUserRegister.getText(), password = txfPasswordRegister.getText(), ced = txfCedRegister.getText(), name = txfNameRegister.getText(), lastName = txfLastNameRegister.getText(), secondLastName = txfSecondLastNameRegister.getText(), phoneNumber = txfPhoneNumberRegister.getText(), email = txfEmailRegister.getText(), role = cbRoleRegister.getValue();
+        if (userName.isBlank() || password.isBlank() || ced.isBlank() || name.isBlank() || lastName.isBlank() || secondLastName.isBlank() || phoneNumber.isBlank() || email.isBlank() || role.isBlank()) {
             System.out.println("You must to fill all the fields");
             return;
         }
         UserDto user = new UserDto();
         user.setEmail(email);
+        user.setIdentification(ced);
+        user.setSecondLastname(secondLastName);
         user.setName(name);
         user.setLastname(lastName);
         user.setPassword(password);
         user.setUsername(userName);
         user.setPhoneNumber(phoneNumber);
         userService.createUser(user);
+    }
+
+    @FXML
+    private void selectPhotoProfile(ActionEvent event) {
     }
 
 }
