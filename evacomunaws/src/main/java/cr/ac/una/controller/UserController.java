a9bd0ee85937f8df326bb80aacc3867d9bed9120
enajera123 <estebannajera42@gmail.com>
2023-08-23 13:18:03 -0600

    [fix]srcipt
    ordene el codigo y los triggers para que se pueda ejecutar todo el script de un solo

--- evacomunaws/src/main/java/cr/ac/una/controller/UserController.java
+++ evacomunaws/src/main/java/cr/ac/una/controller/UserController.java
@@ -4,6 +4,7 @@ import cr.ac.una.dto.UserDto;
 import cr.ac.una.services.UserService;
 import cr.ac.una.util.ResponseWrapper;
 import jakarta.ejb.EJB;
+import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
 @WebService(serviceName = "UserController")
@@ -11,7 +12,7 @@ public class UserController {
 
     @EJB
     private UserService userService;
-
+    @WebMethod(operationName = "createUser")
     public ResponseWrapper createUser(UserDto userDto) {
         return userService.createUser(userDto);
     }
--- evacomunaws/src/main/java/cr/ac/una/services/UserService.java
+++ evacomunaws/src/main/java/cr/ac/una/services/UserService.java
@@ -4,8 +4,10 @@ import cr.ac.una.dto.UserDto;
 import cr.ac.una.util.ResponseWrapper;
 import jakarta.ejb.Local;
 
+
 @Local
 public interface UserService {
+   
     public ResponseWrapper createUser(UserDto userDto);
 
     public ResponseWrapper getUserById(Long id);
