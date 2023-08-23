2b2b0ab769d9e08aa966f34557b15ce210a0baf6
enajera123 <estebannajera42@gmail.com>
2023-08-23 17:40:27 -0600

    [fix]javax
    cambio las dependencias de javax por jakarta en la entidad Company

--- evacomunaws/src/main/java/cr/ac/una/entities/Company.java
+++ evacomunaws/src/main/java/cr/ac/una/entities/Company.java
@@ -2,14 +2,17 @@ package cr.ac.una.entities;
 
 import cr.ac.una.dto.CompanyDto;
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
@@ -21,6 +24,9 @@ import javax.persistence.Table;
     @NamedQuery(name = "Company.findAll", query = "SELECT c FROM Company c"),
     @NamedQuery(name = "Company.findByName", query = "SELECT c FROM Company c WHERE c.name = :name"),
     @NamedQuery(name = "Company.findByEmail", query = "SELECT c FROM Company c WHERE c.email = :email")})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -36,71 +42,10 @@ public class Company implements Serializable {
     @Column(name = "PHOTO")
     private byte[] photo;
 
-    public Company() {
-    }
-
-    public Company(String name) {
-        this.name = name;
-    }
-
     public Company(CompanyDto companyDto) {
         this.name = companyDto.getName();
         this.email = companyDto.getEmail();
         this.photo = companyDto.getPhoto();
     }
 
-    public Company(String name, String email, byte[] photo) {
-        this.name = name;
-        this.email = email;
-        this.photo = photo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public byte[] getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(byte[] photo) {
-        this.photo = photo;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (name != null ? name.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Company)) {
-            return false;
-        }
-        Company other = (Company) object;
-        if ((this.name == null && other.name != null) || (this.name != null && !this.name.equals(other.name))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "cr.ac.una.entities.Company[ name=" + name + " ]";
-    }
 }
