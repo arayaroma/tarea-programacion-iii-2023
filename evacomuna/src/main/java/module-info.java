ed6a27d845ea36154e18b9747224446882dbd33f
Daniel Araya <darayaroma@gmail.com>
2023-08-22 23:21:04 -0600

    [add] module.info requires transitive javafx.graphics

--- evacomuna/src/main/java/module-info.java
+++ evacomuna/src/main/java/module-info.java
@@ -3,17 +3,16 @@ module Evacomuna {
     requires java.logging;
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires jakarta.xml.bind;
     requires jakarta.xml.ws;
     requires org.controlsfx.controls;
-    
+
     opens cr.ac.una.evacomuna to javafx.fxml;
     opens cr.ac.una.util to javafx.fxml;
     opens cr.ac.una.services.impl;
     opens cr.ac.una.controller to javafx.fxml;
-    
-    
+
     exports cr.ac.una.evacomuna;
     exports cr.ac.una.controller;
-
 }
