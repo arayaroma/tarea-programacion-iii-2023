18a405b57aa0927d04325fc36bfd6e068aaeb08e
Daniel Araya <darayaroma@gmail.com>
2023-08-24 00:42:29 -0600

    [UPDATE] scripts for creating the wsdl's

---
+++ create-wsdl.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Check if the script is called with a parameter
+if "%~1"=="" (
+  echo [!] Usage: %0 ^<ControllerName^>
+  exit /b 1
+)
+
+set "controller_name=%~1"
+set "base_url=http://localhost:8080/evacomuna-1.0"
+set "wsdl_endpoint=%controller_name%Controller?wsdl"
+set "url=%base_url%/%wsdl_endpoint%"
+set "output_directory=evacomuna\src\main\resources\cr\ac\una\evacomuna\wsdl"
+set "output_file=%output_directory%\%controller_name%Service.wsdl"
+
+mkdir "%output_directory%" 2>nul
+
+curl -o "%output_file%" "%url%"
+
+echo [+] Request from %url% -^> saved to %output_file%
