368f1c8f856afe43e87fa44f7e5d267a59cd6e1f
Daniel Araya <darayaroma@gmail.com>
2023-08-24 18:47:45 -0600

    [moved] util dir

---
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/util/Message.java
@@ -0,0 +1,34 @@
+package cr.ac.una.evacomuna.util;
+
+import javafx.geometry.Pos;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
+
+/**
+ *
+ * @author estebannajera
+ */
+public class Message {
+
+    public static void showNotification(String header, MessageType type, String content) {
+
+        Notifications notification = Notifications.create()
+                .title(header)
+                .text(content)
+                .graphic(null)
+                .hideAfter(Duration.seconds(5))
+                .darkStyle()
+                .position(Pos.TOP_RIGHT);
+        switch (type) {
+            case ERROR:
+                notification.showError();
+                break;
+            case INFO:
+                notification.show();
+                break;
+            case CONFIRMATION:
+                
+                break;
+        }
+    }
+}
---
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/util/MessageType.java
@@ -0,0 +1,9 @@
+package cr.ac.una.evacomuna.util;
+
+/**
+ *
+ * @author estebannajera
+ */
+public enum MessageType {
+    ERROR,INFO,CONFIRMATION;
+}
