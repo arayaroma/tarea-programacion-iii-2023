235a6f13fa230627a9bf8e4f71a3b1a9ecfa5b38
enajera123 <estebannajera42@gmail.com>
2023-08-27 15:11:12 -0600

    [feat]RoleModule
    Se agrega la pantalla de mantenimiento del role

--- evacomuna/src/main/java/cr/ac/una/evacomuna/controller/RoleModuleController.java
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/controller/RoleModuleController.java
@@ -2,7 +2,15 @@ package cr.ac.una.evacomuna.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
@@ -11,11 +19,53 @@ import javafx.fxml.Initializable;
  */
 public class RoleModuleController implements Initializable {
 
+    @FXML
+    private StackPane parent;
+    @FXML
+    private HBox registerView;
+    @FXML
+    private HBox mainView;
+    @FXML
+    private TextField txfRoleNameRegister;
+    @FXML
+    private ComboBox<?> cbRolesSkillsRegister;
+    @FXML
+    private Button btnAddSkill;
+    @FXML
+    private ListView<?> listRolesRegister;
+    @FXML
+    private Button btnDeleteSkill;
+    @FXML
+    private ListView<?> listSkillsMain;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }    
-    
+    }
+
+    @FXML
+    private void btnNewRole(ActionEvent event) {
+        registerView.toFront();
+    }
+
+    @FXML
+    private void btnBackToMain(ActionEvent event) {
+        mainView.toFront();
+    }
+
+    @FXML
+    private void btnEditRole(ActionEvent event) {
+        registerView.toFront();
+    }
+
+    @FXML
+    private void btnDeleteRole(ActionEvent event) {
+    }
+
+    @FXML
+    private void cbRoles(ActionEvent event) {
+    }
+
 }
