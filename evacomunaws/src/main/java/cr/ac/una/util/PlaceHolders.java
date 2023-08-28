49a81f8cc7ec4a8de7ebf5c489c8e6028886e62a
Daniel Araya <darayaroma@gmail.com>
2023-08-28 01:46:34 -0600

    [add] Constants for the PlaceHolders in the templates

---
+++ evacomunaws/src/main/java/cr/ac/una/util/PlaceHolders.java
@@ -0,0 +1,8 @@
+package cr.ac.una.util;
+
+public class PlaceHolders {
+    public static final String NAME_PLACEHOLDER = "[name]";
+    public static final String USERNAME_PLACEHOLDER = "[username]";
+    public static final String EMAIL_PLACEHOLDER = "[email]";
+    public static final String COMPANY_NAME_PLACEHOLDER = "[company_name]";
+}
