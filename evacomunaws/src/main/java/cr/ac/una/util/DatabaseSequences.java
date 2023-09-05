c430ba174fc6e3633125f20be7d12ce61805595c
Daniel Araya <darayaroma@gmail.com>
2023-09-05 01:08:43 -0600

    [add] DatabaseSequences

---
+++ evacomunaws/src/main/java/cr/ac/una/util/DatabaseSequences.java
@@ -0,0 +1,18 @@
+package cr.ac.una.util;
+
+/**
+ * 
+ * @author arayaroma
+ */
+public interface DatabaseSequences {
+    public static final String SEQ_GENERAL_INFORMATION = "SEQ_GENERAL_INFORMATION";
+    public static final String SEQ_USER = "SEQ_USER";
+    public static final String SEQ_EVALUATED = "SEQ_EVALUATED";
+    public static final String SEQ_EVALUATOR = "SEQ_EVALUATOR";
+    public static final String SEQ_EVALUATION = "SEQ_EVALUATION";
+    public static final String SEQ_POSITION = "SEQ_POSITION";
+    public static final String SEQ_SKILL = "SEQ_SKILL";
+    public static final String SEQ_CHARACTERISTIC = "SEQ_CHARACTERISTIC";
+    public static final String SEQ_CALIFICATION = "SEQ_CALIFICATION";
+    public static final String SEQ_FINAL_CALIFICATION = "SEQ_FINAL_CALIFICATION";
+}
