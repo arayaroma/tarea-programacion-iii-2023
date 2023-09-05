40ce4a338818b4064719514df8a95ba0250a4665
Daniel Araya <darayaroma@gmail.com>
2023-09-05 01:08:24 -0600

    [added] Schema Constant

--- evacomunaws/src/main/java/cr/ac/una/util/Constants.java
+++ evacomunaws/src/main/java/cr/ac/una/util/Constants.java
@@ -1,6 +1,7 @@
 package cr.ac.una.util;
 
 public class Constants {
+    public static final String SCHEMA = "EvaComUNA";
     public static final String COMPANY_NAME = "EvaComUNA";
     public static final String PERSISTENCE_UNIT_NAME = "evacomuna";
     public static final String MAIL_USER = "evacomuna@gmail.com";
