93da7ebe7f535d3194d02862eb9d8eb2aa33c675
Angelo <angelo200284@gmail.com>
2023-09-10 00:48:24 -0600

    [fix] Added XmlSeeAlso annotation so JAXB could look at UserDtoList class when marshalling and unmarshalling. [note] My sanity will never come back

--- evacomunaws/src/main/java/cr/ac/una/util/ResponseWrapper.java
+++ evacomunaws/src/main/java/cr/ac/una/util/ResponseWrapper.java
@@ -1,6 +1,10 @@
 package cr.ac.una.util;
 
 import java.io.Serializable;
+
+import cr.ac.una.dto.UserDto;
+import cr.ac.una.dto.UserDtoList;
+import jakarta.xml.bind.annotation.XmlSeeAlso;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlSeeAlso({UserDtoList.class, UserDto.class})
 public class ResponseWrapper implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer status;
