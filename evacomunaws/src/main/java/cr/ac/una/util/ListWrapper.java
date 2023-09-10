246e5fed121ecb3a031cb233e2b998839037e528
Daniel Araya <darayaroma@gmail.com>
2023-09-10 16:05:34 -0600

    [renamed] JAXBList -> ListWrapper

--- evacomunaws/src/main/java/cr/ac/una/util/ListWrapper.java
+++ evacomunaws/src/main/java/cr/ac/una/util/ListWrapper.java
@@ -4,16 +4,16 @@ package cr.ac.una.util;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.XmlSeeAlso;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 import cr.ac.una.dto.UserDto;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
 @XmlRootElement(name = "list")
 @XmlSeeAlso(UserDto.class)
-public class JAXBList<T> {
-    private List<T> list;
+public class ListWrapper<D> {
+    private List<D> list;
 }
