fe16a10710351cd3f1745bb397120ec119e435b2
Daniel Araya <darayaroma@gmail.com>
2023-09-10 16:04:40 -0600

    [add] script of preloaded positions

---
+++ db/load-positions.sql
@@ -0,0 +1,48 @@
+-- Insert pre-defined positions
+INSERT INTO EVACOMUNA."TBL_POSITION" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    1,
+    'System Administrator',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_POSITION" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    2,
+    'Database Administrator',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_POSITION" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    3,
+    'Backend Developer',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_POSITION" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    4,
+    'Frontend Developer',
+    'ACTIVE',
+    1
+);
