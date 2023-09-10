e68af3d2be5d54fa086851a15fa4c8659d866334
Angelo <angelo200284@gmail.com>
2023-09-10 00:58:22 -0600

    [add] Xml annotations and a method to create userLists.

--- evacomunaws/src/main/java/cr/ac/una/dto/UserDto.java
+++ evacomunaws/src/main/java/cr/ac/una/dto/UserDto.java
@@ -1,7 +1,13 @@
 package cr.ac.una.dto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import cr.ac.una.entities.User;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,24 +18,44 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement(name = "UserDto")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class UserDto {
+    @XmlElement(name = "id")
     private Long id;
+    @XmlElement(name = "username")
     private String username;
+    @XmlElement(name = "password")
     private String password;
+    @XmlElement(name = "name")
     private String name;
+    @XmlElement(name = "lastname")
     private String lastname;
+    @XmlElement(name = "secondLastname")
     private String secondLastname;
+    @XmlElement(name = "identification")
     private String identification;
+    @XmlElement(name = "email")
     private String email;
+    @XmlElement(name = "landlineNumber")
     private String landlineNumber;
+    @XmlElement(name = "phoneNumber")
     private String phoneNumber;
+    @XmlElement(name = "profilePhoto")
     private byte[] profilePhoto;
+    @XmlElement(name = "isActive")
     private String isActive;
+    @XmlElement(name = "isAdmin")
     private String isAdmin;
+    @XmlElement(name = "passwordChanged")
     private String passwordChanged;
+    @XmlElement(name = "activationCode")
     private String activationCode;
+    @XmlElement(name = "position")
     private PositionDto position;
+    @XmlElement(name = "evaluated")
     private List<EvaluatedDto> evaluated;
+    @XmlElement(name = "evaluators")
     private List<EvaluatorDto> evaluators;
     private Long version;
 
@@ -54,5 +80,25 @@ public class UserDto {
         this.passwordChanged = user.getPasswordChanged();
         this.activationCode = user.getActivationCode();
         this.version = user.getVersion();
+    }
+
+    /**
+        * @param users list of users to be converted to dto
+     **/
+    //TODO: Analyze if this belongs in userDto
+    public static UserDtoList fromEntityList(List<User> users) {
+        UserDtoList userDtoList = new UserDtoList();
+        if (users == null || users.isEmpty()) {
+            return userDtoList;
+        }
+
+        List<UserDto> userDtos = new ArrayList<>();
+        for (User user : users) {
+            userDtos.add(new UserDto(user));
+        }
+
+
+        userDtoList.setUserDtoList(userDtos);
+        return userDtoList;
     }
 }
