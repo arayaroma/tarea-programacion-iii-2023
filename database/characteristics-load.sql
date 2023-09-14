24bcf58be27cfda77b39389830c141f98137fe11
Daniel Araya <darayaroma@gmail.com>
2023-09-14 16:24:44 -0600

    [db] charac load script

---
+++ database/characteristics-load.sql
@@ -0,0 +1,95 @@
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    1,
+    'Experiencia en desarrollo de software',
+    1,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    2,
+    'Fuerza de voluntad',
+    1,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    3,
+    'Conocimiento sobre sistemas operativos',
+    2,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    4,
+    'Manejo de procesos e hilos',
+    2,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    5,
+    'Conocimiento tecnico en redes',
+    3,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    6,
+    'Entendimiento protocolos de red',
+    3,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    7,
+    'Experiencia realizando consultas SQL',
+    4,
+    0
+);
+
+INSERT INTO EVACOMUNA."TBL_CHARACTERISTIC" (
+    "ID",
+    "NAME",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    8,
+    'Creacion de consultas eficientes',
+    4,
+    0
+);
