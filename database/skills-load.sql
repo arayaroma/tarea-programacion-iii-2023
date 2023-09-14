902ae6f5e0b43b58b1f0b0b2c7bcf36ce4477277
Daniel Araya <darayaroma@gmail.com>
2023-09-14 16:25:24 -0600

    [db] skills load script

---
+++ database/skills-load.sql
@@ -0,0 +1,47 @@
+INSERT INTO EVACOMUNA."TBL_SKILL" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    1,
+    'Java',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_SKILL" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    2,
+    'Linux',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_SKILL" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    3,
+    'Docker',
+    'ACTIVE',
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_SKILL" (
+    "ID",
+    "NAME",
+    "STATE",
+    "VERSION"
+) VALUES (
+    4,
+    'SQL',
+    'ACTIVE',
+    1
+);
