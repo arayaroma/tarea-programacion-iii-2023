13199ce6e998d189f769443075fb85caf5592126
Daniel Araya <darayaroma@gmail.com>
2023-09-16 13:41:43 -0600

    [REFACTOR] EntityMapper Interface with the implemented methods

--- evacomunaws/src/main/java/cr/ac/una/dto/PositionDto.java
+++ evacomunaws/src/main/java/cr/ac/una/dto/PositionDto.java
@@ -3,6 +3,10 @@ package cr.ac.una.dto;
 import java.util.ArrayList;
 import java.util.List;
 import cr.ac.una.entities.Position;
+import cr.ac.una.entities.Skill;
+import cr.ac.una.entities.User;
+import cr.ac.una.util.EntityMapper;
+import cr.ac.una.util.EntityUtil;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
@@ -18,7 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @XmlRootElement(name = "PositionDto")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class PositionDto {
+public class PositionDto implements EntityMapper<Position, PositionDto> {
 
     private Long id;
     private String name;
@@ -26,6 +30,37 @@ public class PositionDto {
     private Long version;
     private List<UserDto> users;
     private List<SkillDto> skills;
+
+    /**
+     * @param entity entity to be converted
+     * @param dto    dto to be updated
+     * @return dto with the updated information
+     */
+    @Override
+    public PositionDto convertFromEntityToDTO(Position entity, PositionDto dto) {
+        dto.setUsers(EntityUtil.fromEntityList(entity.getUsers(),
+                UserDto.class).getList());
+
+        dto.setSkills(
+                EntityUtil.fromEntityList(entity.getSkills(),
+                        SkillDto.class).getList());
+        return dto;
+    }
+
+    /**
+     * @param entity entity to be converted
+     * @param dto    dto to be updated
+     * @return dto with the updated information
+     */
+    @Override
+    public Position convertFromDTOToEntity(PositionDto dto, Position entity) {
+        entity.setUsers(EntityUtil.fromDtoList(dto.getUsers(),
+                User.class).getList());
+
+        entity.setSkills(EntityUtil.fromDtoList(dto.getSkills(),
+                Skill.class).getList());
+        return entity;
+    }
 
     /**
      * @param position constructor from entity to dto
