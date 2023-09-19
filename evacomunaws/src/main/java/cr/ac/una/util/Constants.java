5de8f5203dc9dab8158bc9249906d0d91984952b
Daniel Araya <darayaroma@gmail.com>
2023-09-19 17:02:31 -0600

    [REBASING]

--- evacomunaws/src/main/java/cr/ac/una/util/Constants.java
+++ evacomunaws/src/main/java/cr/ac/una/util/Constants.java
@@ -8,7 +8,8 @@ public class Constants {
     public static final String SCHEMA = "EvaComUNA";
     public static final String COMPANY_NAME = "EvaComUNA";
     public static final String PERSISTENCE_UNIT_NAME = "evacomuna";
-    public static final String APP_ENDPOINT = "http://localhost:8080/evacomunaws/";
+    public static final String WEB_BASE_DIR = "/evacomuna-1.0/";
+    public static final String PORT = "8080";
     public static final String MAIL_USER = "evacomuna@gmail.com";
     public static final String MAIL_PASSWORD = "abfbwxahufegmtyz";
 }
