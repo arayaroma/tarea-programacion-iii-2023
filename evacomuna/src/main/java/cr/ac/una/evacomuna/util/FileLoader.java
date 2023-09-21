67510c7d48479524dda76337568cc9ccbb413464
Daniel Araya <darayaroma@gmail.com>
2023-09-21 17:43:28 -0600

    [TODO] Implement that class

---
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/util/FileLoader.java
@@ -0,0 +1,10 @@
+package cr.ac.una.evacomuna.util;
+
+/**
+ * 
+ * @author arayaroma
+ */
+public class FileLoader {
+    // TODO: Refactor the methods from Utilities and ImageLoader to this class
+
+}
