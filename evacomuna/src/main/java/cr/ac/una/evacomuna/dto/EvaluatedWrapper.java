bea04b3e3318ca342b7c1617985b574c3e9a2daf
Daniel Araya <darayaroma@gmail.com>
2023-09-24 21:01:41 -0600

    [ADD] added missing fields for this class

--- evacomuna/src/main/java/cr/ac/una/evacomuna/dto/EvaluatedWrapper.java
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/dto/EvaluatedWrapper.java
@@ -1,20 +1,29 @@
 package cr.ac.una.evacomuna.dto;
 
 import cr.ac.una.controller.UserDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import cr.ac.una.controller.EvaluatedDto;
 import cr.ac.una.controller.EvaluationDto;
+import cr.ac.una.controller.EvaluatorDto;
+import cr.ac.una.controller.FinalCalificationDto;
 
 /**
  *
  * @author estebannajera
+ * @author arayaroma
  */
 public class EvaluatedWrapper {
 
-    private UserDto evaluated;
     private Long id;
-    private String finalCalification;
-    private Long version;
+    private UserDto evaluated;
+    private String finalNote;
     private EvaluationDto evaluationDto;
+    private List<EvaluatorDto> evaluators;
+    private List<FinalCalificationDto> finalCalifications;
+    private Long version;
 
     public EvaluatedWrapper(UserDto evaluated) {
         this.evaluated = evaluated;
@@ -22,7 +31,7 @@ public class EvaluatedWrapper {
 
     public EvaluatedWrapper(EvaluatedDto evaluated) {
         this.evaluated = null;
-        this.finalCalification = evaluated.getFinalNote();
+        this.finalNote = evaluated.getFinalNote();
         this.id = evaluated.getId();
         this.version = evaluated.getVersion();
         this.evaluationDto = null;
@@ -34,7 +43,7 @@ public class EvaluatedWrapper {
         evaluatedDto.setEvaluation(evaluationDto);
         evaluatedDto.setId(id);
         evaluatedDto.setVersion(version);
-        evaluatedDto.setFinalNote(finalCalification);
+        evaluatedDto.setFinalNote(finalNote);
         return evaluatedDto;
     }
 
@@ -44,6 +53,13 @@ public class EvaluatedWrapper {
 
     public void setEvaluationDto(EvaluationDto evaluationDto) {
         this.evaluationDto = evaluationDto;
+    }
+
+    public List<EvaluatedWrapper> getAllEvaluated(List<EvaluatedDto> evaluated) {
+        return evaluated
+                .stream()
+                .map(e -> new EvaluatedWrapper(e))
+                .collect(Collectors.toList());
     }
 
 }
