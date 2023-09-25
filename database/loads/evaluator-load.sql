57fef9d9b06272ea6332c8490cec5b85cd2e0ba1
Daniel Araya <darayaroma@gmail.com>
2023-09-24 20:39:11 -0600

    [DB ADD] evaluator-load script

---
+++ database/loads/evaluator-load.sql
@@ -0,0 +1,15 @@
+INSERT INTO EVACOMUNA."TBL_EVALUATOR" (
+    "ID",
+    "EVALUATORID",
+    "ROLE", 
+    "FEEDBACK",
+    "EVALUATEDID",
+    "VERSION"
+) VALUES (
+    1,
+    9999,
+    'SUPERVISOR',
+    'Tremendo',
+    1,
+    1
+);
