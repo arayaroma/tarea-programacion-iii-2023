66d1d5d82b5aa6afe33f4c6538c4a6d5376392aa
Daniel Araya <darayaroma@gmail.com>
2023-09-24 20:39:46 -0600

    [DB ADD] evaluated load script

---
+++ database/loads/evaluated-load.sql
@@ -0,0 +1,14 @@
+INSERT INTO EVACOMUNA."TBL_EVALUATED" (
+    "ID",
+    "EVALUATEDID",
+    "FINALNOTE",
+    "EVALUATIONID",
+    "VERSION"
+) VALUES (
+    1,
+    1,
+    '0',
+    1,
+    1
+);
+
