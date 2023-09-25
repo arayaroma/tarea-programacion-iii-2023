ff6fef68c8a8c12fd1ff7e6714218265ce17cf9b
Daniel Araya <darayaroma@gmail.com>
2023-09-24 20:39:32 -0600

    [DB ADD] Calification load script

---
+++ database/loads/calification-load.sql
@@ -0,0 +1,41 @@
+INSERT INTO EVACOMUNA."TBL_CALIFICATION" (
+    "ID",
+    "CALIFICATION",
+    "SKILLID",
+    "EVALUATORID",
+    "VERSION"
+) VALUES (
+    1,
+    '6',
+    2,
+    1,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_CALIFICATION" (
+    "ID",
+    "CALIFICATION",
+    "SKILLID",
+    "EVALUATORID",
+    "VERSION"
+) VALUES (
+    2,
+    '7',
+    3,
+    1,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_CALIFICATION" (
+    "ID",
+    "CALIFICATION",
+    "SKILLID",
+    "EVALUATORID",
+    "VERSION"
+) VALUES (
+    3,
+    '8',
+    4,
+    1,
+    1
+);
