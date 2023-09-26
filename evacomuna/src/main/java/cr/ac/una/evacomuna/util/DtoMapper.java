989d8ef4c49ecf7fe229d833904cf1bc15666354
Daniel Araya <darayaroma@gmail.com>
2023-09-26 03:21:58 -0600

    [GOD] GOLD STANDARD DtoMapper class for mapping gen-classes to client's dto classes
    Honestly I'm proud of myself with this commit

---
+++ evacomuna/src/main/java/cr/ac/una/evacomuna/util/DtoMapper.java
@@ -0,0 +1,60 @@
+package cr.ac.una.evacomuna.util;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * @author arayaroma
+ */
+public interface DtoMapper<G, D> {
+    D convertFromGeneratedToDTO(G generated, D dto);
+
+    G convertFromDTOToGenerated(D dto, G generated);
+
+    public static <E, D> ListWrapper<D> fromGeneratedList(List<E> generated, Class<D> dtoClass) {
+        ListWrapper<D> listWrapper = new ListWrapper<>();
+        if (generated == null || generated.isEmpty()) {
+            return listWrapper;
+        }
+        List<D> dtos = generated.stream()
+                .map(gen -> convertToDto(gen, dtoClass))
+                .collect(Collectors.toList());
+        listWrapper.setList(dtos);
+        return listWrapper;
+    }
+
+    public static <E, D> ListWrapper<E> fromDtoList(List<D> dtos, Class<E> generatedClass) {
+        ListWrapper<E> listWrapper = new ListWrapper<>();
+        if (dtos == null || dtos.isEmpty()) {
+            return listWrapper;
+        }
+        List<E> generated = dtos.stream()
+                .map(dto -> convertToGenerated(dto, generatedClass))
+                .collect(Collectors.toList());
+        listWrapper.setList(generated);
+        return listWrapper;
+    }
+
+    public static <T, D> D convertToDto(T generated, Class<D> dtoClass) {
+        try {
+            dtoClass.getConstructor(generated.getClass());
+            Constructor<D> constructor = dtoClass.getConstructor(generated.getClass());
+            D dto = constructor.newInstance(generated);
+            return dto;
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting Generated to DTO", e);
+        }
+    }
+
+    public static <T, D> T convertToGenerated(D dto, Class<T> generatedClass) {
+        try {
+            Constructor<T> constructor = generatedClass.getConstructor(dto.getClass());
+            T gen = constructor.newInstance(dto);
+            return gen;
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting DTO to Generated", e);
+        }
+    }
+}
