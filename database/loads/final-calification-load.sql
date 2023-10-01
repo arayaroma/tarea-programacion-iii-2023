b7c058d7dd3347b278c0424fa6fa805062122d80
Daniel Araya <darayaroma@gmail.com>
2023-10-01 04:14:52 -0600

    [DB ADDED] final calification load script

---
+++ database/loads/final-calification-load.sql
@@ -0,0 +1,271 @@
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    1,
+    '3',
+    '3',
+    1,
+    2,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    2,
+    '4',
+    '4',
+    2,
+    3,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    3,
+    '2',
+    '2',
+    3,
+    8,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    4,
+    '3',
+    '3',
+    4,
+    9,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    5,
+    '1',
+    '1',
+    5,
+    3,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    6,
+    '2',
+    '2',
+    6,
+    4,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    7,
+    '2',
+    '2',
+    7,
+    1,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    8,
+    '4',
+    '4',
+    8,
+    4,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    9,
+    '3',
+    '3',
+    9,
+    5,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    10,
+    '3',
+    '3',
+    10,
+    7,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    11,
+    '3',
+    '3',
+    11,
+    9,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    12,
+    '1',
+    '1',
+    12,
+    1,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    13,
+    '2',
+    '2',
+    13,
+    4,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    14,
+    '3',
+    '3',
+    14,
+    5,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    15,
+    '4',
+    '4',
+    15,
+    7,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    16,
+    '2',
+    '2',
+    16,
+    9,
+    1
+);
+
+INSERT INTO EVACOMUNA."TBL_FINAL_CALIFICATION" (
+    "ID",
+    "FINALNOTE",
+    "AVERAGE",
+    "EVALUATEDID",
+    "SKILLID",
+    "VERSION"
+) VALUES (
+    17,
+    '3',
+    '3',
+    17,
+    9,
+    1
+);
