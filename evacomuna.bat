5853a05a5e3e887bbd87ef60d5948b7e17c5483b
Daniel Araya <darayaroma@gmail.com>
2023-10-02 01:14:47 -0600

    [MODIFY] BAT SCRIPT FOR WINDOWS

--- evacomuna.bat
+++ evacomuna.bat
@@ -1,12 +1,11 @@
 @echo off
 
-set "JAVA_HOME=%~dp0..\"
-
 echo Running evacomuna.jar...
-"%JAVA_HOME%\bin\java" --module-path evacomuna\javafx-sdk-21\lib --add-modules javafx.controls,javafx.fxml -jar evacomuna\evacomuna.jar
+"%JAVA_HOME%\bin\java" --module-path "C:\Users\DanyG\repos\tarea-programacion-iii-2023\evacomuna\javafx-sdk-21\lib" --add-modules javafx.controls,javafx.fxml -jar evacomuna\evacomuna.jar
 
 if %ERRORLEVEL% EQU 0 (
   echo Execution complete.
 ) else (
   echo Execution failed.
 )
+
