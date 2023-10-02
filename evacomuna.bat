c800e80722c35869485c2eebdeb17b8dc894019a
Daniel Araya <darayaroma@gmail.com>
2023-10-02 01:44:19 -0600

    [Windows Script] Fix Environment Variable JAVAFX_SDK

--- evacomuna.bat
+++ evacomuna.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Running evacomuna.jar...
-"%JAVA_HOME%\bin\java" --module-path "C:\Users\DanyG\repos\tarea-programacion-iii-2023\evacomuna\javafx-sdk-21\lib" --add-modules javafx.controls,javafx.fxml -jar evacomuna\evacomuna.jar
+"%JAVA_HOME%\bin\java" --module-path "%JAVAFX_SDK%\lib" --add-modules javafx.controls,javafx.fxml -jar evacomuna\evacomuna.jar
 
 if %ERRORLEVEL% EQU 0 (
   echo Execution complete.
